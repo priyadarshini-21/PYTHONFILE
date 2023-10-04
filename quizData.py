302c2b79d8c2122d1387b5d94d7ad0e7682c26f9
Priyadarshini-21 <priyadarshinisingh2001@gmail.com>
2023-10-04 22:19:30 +0530

    1st

---
+++ quizData.py
@@ -0,0 +1,52 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "London", "Berlin", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the largest planet of our solar system?",
+        "choices": ["Earth", "Saturn", "Mars", "Jupiter"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "What is the chemical symbol of gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is known as the 'Land of the Rising Sun?",
+        "choices": ["China", "Japan", "South Korea", "Madrid"],
+        "answer": "Japan"
+    },
+    {
+        "question": "What is actually electricity?",
+        "choices": ["A flow of water", "A flow of air", " A flow of electrons", "A flow of atoms"],
+        "answer": "A flow of electrons"
+    },
+    {
+        "question": "Which is the easiest way to tell the age of many trees?",
+        "choices": ["To measure the width of the tree", "To measure the height of the tree", "To count the rings on the trunk", "To count the number of leaves"],
+        "answer": "To count the rings on the trunk"
+    },
+    {
+        "question": "What do we call a newly hatched butterfly?",
+        "choices": ["A moth", "A butter", "A caterpillar", "A chrysalis"],
+        "answer": "A caterpillar"
+    },
+    {
+        "question": "What is the main component of the sun?",
+        "choices": ["Liquid lava", "Gas", "Molten iron", " Rock"],
+        "answer": "Gas"
+    },
+    {
+        "question": "Which two months are named after Emperors of the Roman Empire?",
+        "choices": ["January and February", "March and April", "May and June", "July and August"],
+        "answer": "July and August"
+    },
+    {
+        "question": "Which of the following animals can run the fastest?",
+        "choices": ["Cheetah", "Leopard", "Tiger", "Lion"],
+        "answer": "Leopard"
+    },
+]
